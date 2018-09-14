1b055bb0cef0c641e5146b5f52619f97ea7aa58e
Robson Rodrigues <bobson278@gmail.com>
2018-09-14 13:39:03 -0300

    ajuste no arquivo de testes

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -2,8 +2,10 @@ const { expect } = require('code')
 const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 
+const { soma } = require('../soma')
+
 lab.test('returns true when 1 + 1 equals 2', (done) => {
 
     expect(soma(1,1)).to.equal(2)
     
-});
+})
