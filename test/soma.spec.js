2ee01edaa94dccc85bfa3717096b066865819e85
Robson Rodrigues <bobson278@gmail.com>
2018-09-14 13:43:26 -0300

    add novo teste

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -4,8 +4,14 @@ const lab = exports.lab = Lab.script()
 
 const { soma } = require('../soma')
 
-lab.test('returns true when 1 + 1 equals 2', (done) => {
+lab.test('returns true when 1 + 1 equals 2', () => {
 
     expect(soma(1,1)).to.equal(2)
+    
+})
+
+lab.test('Rodar sem parametros', () => {
+
+    expect(soma()).to.equal(0)
     
 })
